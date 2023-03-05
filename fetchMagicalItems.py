f9adc9c9127d410b93366dd54f9e524f81ba957e
lindebros <math.linde1994@gmail.com>
2023-03-05 17:18:55 +0100

    Tweaked the fetching of magical items. It now fetches description of each item

--- fetchMagicalItems.py
+++ fetchMagicalItems.py
@@ -11,6 +11,8 @@ sourceDict = {}
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
 
+itemBaseUrl = "http://dnd5e.wikidot.com"
+
 # sources
 sources = soup.find_all("table")[0].find_all("tr")
 for s in sources:
@@ -25,13 +27,23 @@ levels = [l.get_text() for l in soup.find("ul", class_ = "yui-nav").find_all("em
 tabs = soup.find("div", class_ = "yui-content").find_all("table")
 for ti in range(len(tabs)):
     lvl = levels[ti]
+    print("Fetching " + lvl + " magical items!")
+    i = 0
     for item in tabs[ti].find_all("tr")[1:]:
+        i += 1
+        if i % 25 == 0:
+            print(str(i) + " " + levels[ti]  + " magical items parsed!")
         name = item.find_all("td")[0].get_text()
         type = item.find_all("td")[1].get_text()
         attuned = item.find_all("td")[2].get_text() != '-'
         source = ", ".join([sourceDict[s] if s != 'WGE' else sourceDict["XGE"] for s in item.find_all("td")[3].get_text().split(", ") ])
 
-        results.append([name, lvl, type, attuned, source])
+        itemUrl = itemBaseUrl + item.find_all("td")[0].find("a")['href']
+        itemResponse = requests.get(itemUrl)
+        isoup = BeautifulSoup(itemResponse.text, "html.parser")
+        desc = " ".join([p.get_text() for p in isoup.find_all("div", id="page-content")[0].find_all("p")[2:]])
+
+        results.append([name, lvl, type, attuned, desc, source])
 
 
 with open("items.csv", 'w', encoding='utf-8', newline='') as toWrite:
